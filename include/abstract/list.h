2d2b005838967c4c04b175d81d4b4a36ff5ccb85
Fabio Buracchi <45599613+Sph00b@users.noreply.github.com>
2020-09-27 04:03:59 +0200

    Update interface and comments

--- include/abstract/list.h
+++ include/abstract/list.h
@@ -25,7 +25,7 @@ extern ds_list_t ds_list_init(void (*destroy)(void*));
 /*
 * Destroy a list object.
 *
-* @param	this	-	the list to destroy.
+* @param	this	-	the list object.
 * 
 * @return	0 on success; non-zero otherwise.
 */
@@ -39,7 +39,7 @@ extern int ds_list_destroy(const ds_list_t this);
 * Returns a reference to the first element in the list.
 * Calling ds_list_get_front on an empty list is undefined.
 *
-* @param	this	-	the list containing the element.
+* @param	this	-	the list object.
 * @param	element	-	the pointer that will reference the first element.
 * 
 * @return	0 on success; non-zero otherwise.
@@ -51,7 +51,7 @@ extern int ds_list_get_front(const ds_list_t this,
 * Returns a reference to the last element in the list.
 * Calling ds_list_get_back on an empty list is undefined.
 *
-* @param	this	-	the list containing the element.
+* @param	this	-	the list object.
 * @param	element -	the pointer that will reference the last element.
 * 
 * @return	0 on success; non-zero otherwise.
@@ -66,7 +66,7 @@ extern int ds_list_get_back(const ds_list_t this,
 /*
 * Checks if the list has no elements.
 *
-* @param	this	 -	the list to check.
+* @param	this	 -	the list object.
 * @param	is_empty -	the pointer that will reference the status of the list.
 * 
 * @return	0 on success; non-zero otherwise.
@@ -76,7 +76,7 @@ extern int ds_list_is_empty(const ds_list_t this, bool* is_empty);
 /*
 * Returns the number of elements in the list.
 *
-* @param	this	-	the list to check.
+* @param	this	-	the list object.
 * @param	size	-	the pointer that will reference the length of the list.
 * 
 * @return	0 on success; non-zero otherwise.
@@ -91,7 +91,7 @@ extern int ds_list_get_size(const ds_list_t this, int* size);
 * Erases all elements from the list. After this call, ds_list_get_size() returns
 * zero.
 *
-* @param	this	-	the list to clear.
+* @param	this	-	the list object.
 * 
 * @return	0 on success; non-zero otherwise.
 */
@@ -100,9 +100,9 @@ extern int ds_list_clean(const ds_list_t this);
 /*
 * Inserts elements in the list before the specified position.
 *
-* @param	this		-	the list where the element will be inserted.
-* @param	element		-	element before which the content will be inserted.
-* @param	value		-	element value to insert.
+* @param	this	-	the list object.
+* @param	element	-	element before which the content will be inserted.
+* @param	value	-	element value to insert.
 * 
 * @return	0 on success; non-zero otherwise.
 */
@@ -112,7 +112,7 @@ extern int ds_list_insert(const ds_list_t this, const ds_list_element_t element,
 /*
 * Erases the specified elements from the list.
 *
-* @param	this	-	the list where the element will be erased.
+* @param	this	-	the list object.
 * @param	element	-	the element to remove.
 * @return	0 on success; non-zero otherwise.
 */
@@ -121,7 +121,7 @@ extern int ds_list_erase(const ds_list_t this, const ds_list_element_t element);
 /*
 * Appends the given element value to the beginning of the list.
 *
-* @param	this	-	the list where the element will be inserted.
+* @param	this	-	the list object.
 * @param	value	-	the value of the element to prepend.
 * @return	0 on success; non-zero otherwise.
 */
@@ -131,7 +131,7 @@ extern int ds_list_push_front(const ds_list_t this, const void* value);
 * Removes the first element of the list. If there are no elements in the list,
 * the behavior is undefined.
 *
-* @param	this	-	the list where the element will be removed.
+* @param	this	-	the list object.
 * @return	0 on success; non-zero otherwise.
 */
 extern int ds_list_pop_front(const ds_list_t this);
@@ -139,7 +139,7 @@ extern int ds_list_pop_front(const ds_list_t this);
 /*
 * Appends the given element value to the end of the list.
 *
-* @param	this	-	the list where the element will be inserted.
+* @param	this	-	the list object.
 * @param	value	-	the value of the element to append.
 * @return	0 on success; non-zero otherwise.
 */
@@ -149,7 +149,7 @@ extern int ds_list_push_back(const ds_list_t this, const void* value);
 * Removes the last element of the list. If there are no elements in the list,
 * the behavior is undefined.
 *
-* @param	this	-	the list where the element will be removed.
+* @param	this	-	the list object.
 * @return	0 on success; non-zero otherwise.
 */
 extern int ds_list_pop_back(const ds_list_t this);
@@ -161,7 +161,7 @@ extern int ds_list_pop_back(const ds_list_t this);
 * If the current size is less than count, additional default-inserted elements
 * and additional copies of value are appended.
 *
-* @param	this	-	the list to clear.
+* @param	this	-	the list object.
 * @param	size	-	new size of the list.
 * @param	value	-	the value to initialize the new elements with.
 * @return	0 on success; non-zero otherwise.
@@ -172,7 +172,7 @@ extern int ds_list_resize(const ds_list_t this, int size, void* value);
 * Exchanges the contents of the list with those of other. Does not invoke any
 * move, copy, or swap operations on individual elements.
 *
-* @param	this	-	the list to exchange contents with.
+* @param	this	-	the list object.
 * @param	other	-	the other list to exchange the contents with.
 * @return	0 on success; non-zero otherwise.
 */
@@ -192,10 +192,10 @@ extern int ds_list_swap(const ds_list_t this, const ds_list_t other);
 * elements from this shall always precede the elements from other, and the order
 * of equivalent elements of this and other does not change.
 *
-* @param this	-	the list to merge with.
-* @param other	-	another list to merge.
-* @param comp	-	binary function which set a boolean as true if the first
-*					argument is less than the second.
+* @param	this	-	the list object.
+* @param	other	-	another list to merge.
+* @param	comp	-	binary function which set a boolean as true if the first
+*						argument is less than the second.
 * @return 0 on success; non-zero otherwise.
 */
 extern int ds_list_merge(const ds_list_t this, const ds_list_t other,
@@ -210,10 +210,10 @@ extern int ds_list_merge(const ds_list_t this, const ds_list_t other,
 * The behavior is undefined if position is an iterator in the range
 * [first,last).
 *
-* @param this	-	the list
-* @param other	-	another list to transfer the content from.
-* @parma pos	-	element before which the content will be inserted.
-* @param first, last -	the range of elements to transfer from other to this.
+* @param	this	-	  the list object.
+* @param	other	-	  another list to transfer the content from.
+* @parma	pos		-	  element before which the content will be inserted.
+* @param	first, last - the range of elements to transfer from other to this.
 *
 * @return 0 on success; non-zero otherwise.
 */
@@ -222,9 +222,9 @@ extern int ds_list_splice(const ds_list_t this);
 /*
 * Removes all elements satisfying specific criteria.
 *
-* @param this	-	the list
-* @param p		-	unary function which set a boolean as true if the element
-*					should be removed.
+* @param	this	-	the list object.
+* @param	p		-	unary function which set a boolean as true if the
+*						element should be removed.
 *
 * @return 0 on success; non-zero otherwise.
 */
@@ -234,7 +234,7 @@ extern int ds_list_remove_if(const ds_list_t this,
 /*
 * Reverses the order of the elements in the container.
 *
-* @param this	-	the list
+* @param	this	-	the list object.
 *
 * @return 0 on success; non-zero otherwise.
 */
@@ -244,9 +244,9 @@ extern int ds_list_reverse(const ds_list_t this);
 * Removes all consecutive duplicate elements from the container. Only the first
 * element in each group of equal elements is left.
 *
-* @param this	-	the list
-* @param comp	-	binary function which set a boolean as true if the first
-*					argument is equal to the second.
+* @param	this	-	the list object.
+* @param	comp	-	binary function which set a boolean as true if the first
+*						argument is equal to the second.
 *
 * @return 0 on success; non-zero otherwise.
 */
@@ -258,9 +258,9 @@ extern int ds_list_unique(const ds_list_t this,
 * preserved.
 * If an error occurs, the order of elements in this is unspecified.
 *
-* @param this	-	the list
-* @param comp	-	binary function which set a boolean as true if the first
-*					argument is less than the second.
+* @param	this	-	the list object.
+* @param	comp	-	binary function which set a boolean as true if the first
+*						argument is less than the second.
 *
 * @return 0 on success; non-zero otherwise.
 */
--- include/abstract/stack.h
+++ include/abstract/stack.h
@@ -2,57 +2,106 @@
 
 #include <stdbool.h>
 
+/*******************************************************************************
+*								  Member types								   *
+*******************************************************************************/
+
 typedef void* ds_stack_t;
+
+/*******************************************************************************
+*							    Member functions							   *
+*******************************************************************************/
 
 /*
 * Return an initialized stack object.
 * 
-* @return the initialized stack on success; NULL otherwise.
+* @param	destroy	-	a destroy function, NULL is default.
+* 
+* @return	the initialized stack on success; NULL otherwise.
 */
 extern ds_stack_t ds_stack_init(void (*destroy)(void*));
 
 /*
 * Destroy a stack object.
 * 
-* @param stack - the stack to destroy.
-* @return 0 on success; non-zero otherwise.
+* @param	this	-	the stack object.
+* 
+* @return	0 on success; non-zero otherwise.
 */
-extern int ds_stack_destroy(const ds_stack_t stack);
+extern int ds_stack_destroy(const ds_stack_t this);
+
+/*******************************************************************************
+*								 Element access								   *
+*******************************************************************************/
 
 /*
-* Set the value referenced by is_empty as true if the stack is empty or as flase
+* Returns reference to the top element in the stack. This is the most recently
+* pushed element. This element will be removed on a call to pop().
+*
+* @param	this	-	the stack object.
+* @param	value	-	the pointer that will reference the top element value.
+* 
+* @return 0 on success; non-zero otherwise.
+*/
+extern int ds_stack_top(const ds_stack_t this, void** value);
+
+
+/*******************************************************************************
+*									Capacity								   *
+*******************************************************************************/
+
+/*
+* Set the value referenced by is_empty as true if the stack is empty or as false
 * otherwise.
 * 
-* @param stack - the stack to check.
-* @param is_empty - the pointer that will reference the state of the stack.
+* @param	this	-	the stack object.
+* @param	is_empty -	the pointer that will reference the state of the stack.
+* 
 * @return 0 on success; non-zero otherwise.
 */
-extern int ds_stack_is_empty(const ds_stack_t stack, bool* is_empty);
+extern int ds_stack_is_empty(const ds_stack_t this, bool* is_empty);
 
 /*
-* Push an item onto a stack.
-* 
-* @param stack - the stack to push the item onto.
-* @param item - the item to push onto the stack.
-* @return 0 on success; non-zero otherwise.
+* Returns the number of elements in the stack.
+*
+* @param	this	-	the stack object.
+* @param	size	-	the pointer that will reference the length of the stack.
+*
+* @return	0 on success; non-zero otherwise.
 */
-extern int ds_stack_push(const ds_stack_t stack, const void* item);
+extern int ds_stack_get_size(const ds_list_t this, int* size);
+
+
+/*******************************************************************************
+*									Modifiers								   *
+*******************************************************************************/
 
 /*
-* Pop the first item off a stack.
+* Pushes the given element value to the top of the stack.
 * 
-* @param stack - the stack to remove the top item from.
-* @param item - the pointer that will reference the item, if the parameter
-* is NULL the item is ignored.
-* @return 0 on success; non-zero otherwise.
+* @param	this	-	the stack object.
+* @param	value	-	the value of the element to push.
+* 
+* @return	0 on success; non-zero otherwise.
 */
-extern int ds_stack_pop(const ds_stack_t stack, void** item);
+extern int ds_stack_push(const ds_stack_t this, const void* value);
 
 /*
-* Return the item onto a stack.
+* Removes the top element from the stack.
 * 
-* @param stack - the stack to return the top item from.
-* @param item - the pointer that will reference the item.
-* @return 0 on success; non-zero otherwise.
+* @param	this	-	the stack object.
+* @param	value	-	the pointer that will reference the value of the popped
+*						element, if the parameter is NULL the value is ignored.
+* 
+* @return	0 on success; non-zero otherwise.
 */
-extern int ds_stack_top(const ds_stack_t stack, void** item);
+extern int ds_stack_pop(const ds_stack_t this, void** value);
+
+/*
+* Exchanges the contents of the stack with those of other.
+*
+* @param	this	-	the stack object.
+* @param	other	-	the other stack to exchange the contents with.
+* @return	0 on success; non-zero otherwise.
+*/
+extern int ds_stack_swap(const ds_stack_t this, const ds_stack_t other);
