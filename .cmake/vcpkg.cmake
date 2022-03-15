fe76e5ecd002045ba77e64078d87855f9a899998
Fabio Buracchi <45599613+buracchi@users.noreply.github.com>
2022-03-15 04:29:11 +0100

    Update vcpkg.cmake

--- .cmake/vcpkg.cmake
+++ .cmake/vcpkg.cmake
@@ -4,3 +4,13 @@ if(EXISTS ${VCPKG_TOOLCHAIN_FILE})
     message(STATUS "Using vcpkg toolchain")
     set(CMAKE_TOOLCHAIN_FILE ${VCPKG_TOOLCHAIN_FILE} CACHE STRING "Vcpkg toolchain file")
 endif()
+
+file(TIMESTAMP "${CMAKE_CURRENT_SOURCE_DIR}/vcpkg.json" vcpkg_json_timestamp)
+
+if(VCPKG_JSON_TIMESTAMP_PREV STREQUAL vcpkg_json_timestamp)
+    set(VCPKG_MANIFEST_INSTALL OFF CACHE STRING "" FORCE)
+else()
+    set(VCPKG_MANIFEST_INSTALL ON CACHE STRING "" FORCE)
+endif()
+
+set(VCPKG_JSON_TIMESTAMP_PREV "${vcpkg_json_timestamp}" CACHE STRING "" FORCE)
